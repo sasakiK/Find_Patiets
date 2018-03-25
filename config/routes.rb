5806dbe99130a5084f914bd76060087204334a97
sasakiK <sasakikensuke@ssk-MacBook-Air.local>
2018-03-26 00:33:26 +0900

    search form modify

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get "result" => "home#result"
 
   resources :results, only: [:index]
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
