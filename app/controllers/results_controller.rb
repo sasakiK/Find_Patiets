42b7347be134fceb0af0ab34243281fcaeccf71f
sasakiK <sasakikensuke@ssk-MacBook-Air.local>
2018-04-09 00:07:02 +0900

    DLページへのroutesなど追加

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,7 +1,9 @@
 require 'csv'
 
 class ResultsController < ApplicationController
+
   def index
+    params[:search] ||= ""
     @results = Result.search(params[:search])
     temp_result = @results
     respond_to do |format|
@@ -9,6 +11,11 @@ class ResultsController < ApplicationController
       format.csv {send_csv temp_result.to_csv}
     end
     @judge = @results.length
+  end
+
+
+  def download
+    @patients = Result.find_by(id: params[:id])
   end
 
 end
